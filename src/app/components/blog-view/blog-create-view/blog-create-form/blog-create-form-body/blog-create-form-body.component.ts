352d460d230e0acd6fabccfc908d68f487a9a6e8
anontyro <alexwilkinson@gmail.com>
2017-11-16 22:54:34 +0800

    working on getting the markup editor working found a method that works generally but loses focus when buttons are pressed

--- src/app/components/blog-view/blog-create-view/blog-create-form/blog-create-form-body/blog-create-form-body.component.ts
+++ src/app/components/blog-view/blog-create-view/blog-create-form/blog-create-form-body/blog-create-form-body.component.ts
@@ -9,9 +9,55 @@ export class BlogCreateFormBodyComponent implements OnInit {
   @Input()
   @Output()
   public blogText;
+
+  public blogTextArea: HTMLTextAreaElement = <HTMLTextAreaElement> document.getElementById('blog-body');
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  public getSelection(before: string, after: string) {
+    console.log("clicked");
+	  let textarea: HTMLTextAreaElement = this.blogTextArea;
+    if (document.activeElement === textarea) {
+      if (typeof textarea.selectionStart =='number' && typeof textarea.selectionEnd =='number') {
+        
+        let start = textarea.selectionStart;
+        let end = textarea.selectionEnd;
+
+        let selectedText = textarea.value.slice(start, end);
+        let beforeMyText = textarea.value.slice(0,start);
+        let afterMyText = textarea.value.slice(end);
+
+        let newText = beforeMyText + before + selectedText + after + afterMyText;
+
+        textarea.value = newText;
+
+      }
+    }
+  }
+  
+
+
+
 }
+/*
+ createListElement() {
+    if(document.activeElement === textarea) {
+        if(typeof textarea.selectionStart == 'number' && typeof textarea.selectionEnd == 'number') {
+            // All browsers except IE
+            var start = textarea.selectionStart;
+            var end = textarea.selectionEnd;
+
+            var selectedText = textarea.value.slice(start, end);
+            var before = textarea.value.slice(0, start);
+            var after = textarea.value.slice(end);
+
+            var text = before + '- ' + selectedText + after;
+            textarea.value = text;
+       }
+   }
+}
+    }
+*/
